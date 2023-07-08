4d8148d2d63fbb4064c11b72ac1476e88866ab4b
HauKlumsy <ninjago8365@gmail.com>
2023-07-08 16:39:33 -0700

    Implement hamburger
    - Fixed the issue where the links are stacked on top of each other
    
    - Got installed a dependencies for hamburger
    
    - Need to make the hamburger stay at the top of the page, the 2nd hamburger from the side when pressing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,118 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Divide as Hamburger } from 'hamburger-react'
+
+import images from '../../constants';
+import './Navbar.scss';
+
+// const Burger = ({ open, setOpen }) => {
+//   const links = [
+//     {
+//       name: 'Home',
+//       href: '#home',
+//     },
+//     {
+//       name: 'About',
+//       href: '#about',
+//     },
+//     {
+//       name: 'Experience',
+//       href: '#experience',
+//     },
+//     {
+//       name: 'Skills',
+//       href: '#skills',
+//     },
+//     {
+//       name: 'Contact',
+//       href: '#contact',
+//     },
+//   ];
+
+//   return (
+//     <button
+//       type="button"
+//       className="hamburger"
+//       onClick={() => setOpen(!open)}
+//     >
+//       {open ? <span>X</span> : <span>-</span>}
+//       {open ? (
+//         <ul>
+//           {links.map(link => (
+//             <li key={link.name}>
+//               <a href={link.href}>{link.name}</a>
+//             </li>
+//           ))}
+//         </ul>
+//       ) : null}
+//     </button>
+//   );
+// };
+
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {['home', 'about', 'experience', 'skills', 'contact'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className='app__navbar-menu'>
+
+        <Hamburger 
+          toggle={() => setOpen(true)} 
+          toggled={open}
+          size={20}
+          distance='md'  
+          />
+        {/* when the menu is open, show links */}
+        {open && (
+          <div>
+            <Hamburger
+            toggle={() => setOpen(false)} 
+            toggled={open}
+            size={20}
+            distance='md'
+            />
+          </div>
+        )}
+      </div>
+
+
+      {/* <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div> */}
+
+
+
+    </nav>
   )
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,39 @@
+import api from '../assets/api.png';
+import bgIMG from '../assets/bgIMG.png';
+import bgWhite from '../assets/bgWhite.png';
+import cpp from '../assets/cpp.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import git from '../assets/git.png';
+import html from '../assets/html.png';
+import js from '../assets/javascript.png';
+import mobile from '../assets/mobile.png';
+import node from '../assets/node.png';
+import py from '../assets/python.png';
+import react from '../assets/react.png';
+import sass from '../assets/sass.png';
+import ts from '../assets/typescript.png';
+import vue from '../assets/vue.png';
+import logo from '../assets/logo.png';
+
+const images = {
+    api,
+    bgIMG,
+    bgWhite,
+    cpp,
+    css,
+    figma,
+    git,
+    html,
+    js,
+    mobile,
+    node,
+    py,
+    react,
+    sass,
+    ts,
+    vue,
+    logo,
+};
+
+export default images;
