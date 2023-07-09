00a55f2a4edd165b7cd08780c5ea0776e3909fff
HauKlumsy <ninjago8365@gmail.com>
2023-07-09 00:31:27 -0700

    Update burger icon
    - Added framer motion dependency
    - Remove the burger-react

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,9 @@
 import React, { useState } from 'react';
-import { Divide as Hamburger } from 'hamburger-react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import images from '../../constants';
 import './Navbar.scss';
-
-// const Burger = ({ open, setOpen }) => {
-//   const links = [
-//     {
-//       name: 'Home',
-//       href: '#home',
-//     },
-//     {
-//       name: 'About',
-//       href: '#about',
-//     },
-//     {
-//       name: 'Experience',
-//       href: '#experience',
-//     },
-//     {
-//       name: 'Skills',
-//       href: '#skills',
-//     },
-//     {
-//       name: 'Contact',
-//       href: '#contact',
-//     },
-//   ];
-
-//   return (
-//     <button
-//       type="button"
-//       className="hamburger"
-//       onClick={() => setOpen(!open)}
-//     >
-//       {open ? <span>X</span> : <span>-</span>}
-//       {open ? (
-//         <ul>
-//           {links.map(link => (
-//             <li key={link.name}>
-//               <a href={link.href}>{link.name}</a>
-//             </li>
-//           ))}
-//         </ul>
-//       ) : null}
-//     </button>
-//   );
-// };
 
 
 const Navbar = () => {
@@ -67,51 +24,27 @@ const Navbar = () => {
       </ul>
 
       <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setOpen(true)} />
 
-        <Hamburger 
-          toggle={() => setOpen(true)} 
-          toggled={open}
-          size={20}
-          distance='md'  
-          />
-        {/* when the menu is open, show links */}
         {open && (
-          <div>
-            <Hamburger
-            toggle={() => setOpen(false)} 
-            toggled={open}
-            size={20}
-            distance='md'
-            />
-          </div>
-        )}
-      </div>
-
-
-      {/* <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
-
-        {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
             transition={{ duration: 0.85, ease: 'easeOut' }}
           >
-            <HiX onClick={() => setToggle(false)} />
+            <HiX onClick={() => setOpen(false)} />
             <ul>
-              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
-                </li>
-              ))}
+            {['home', 'about', 'experience', 'skills', 'contact'].map((item) => (
+              <li key={item}>
+                <a href={`#${item}`} onClick={() => setOpen(false)}>
+                  {item}
+                </a>
+              </li>
+            ))}
             </ul>
           </motion.div>
         )}
-      </div> */}
-
-
-
+        
+      </div>
     </nav>
   )
 }
